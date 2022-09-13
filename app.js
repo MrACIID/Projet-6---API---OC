52c3747c007a69bb52fbaaaf55f69699549677ab
unknown <acidddropdesign@gmail.com>
2022-09-13 16:20:18 +0200

    model,route,controller sauce , multer image

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-
+const path = require('path');
+const sauceRoutes = require('./routes/Sauce');
 const userRoutes = require('./routes/user');
 
 const app = express();
@@ -22,6 +22,10 @@ app.use((req, res, next) => {
   next();
 });
 
+
 app.use('/api/auth/',userRoutes);
+app.use('/api/sauces',sauceRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 module.exports = app;
